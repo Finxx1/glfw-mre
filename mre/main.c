3d7b9c7eb393501bf8bfa2886ab37f5d56db81bd
Finxx <notfinxx@gmail.com>
2024-02-19 19:32:42 -0600

    Initial Commit

---
+++ mre/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  mre
+//
+//  Created by Finxx1 on 2/19/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
