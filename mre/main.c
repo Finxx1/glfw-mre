d157e1d22284d081fc6fba13c8af7b4d9656235d
Finxx <notfinxx@gmail.com>
2024-02-19 19:36:59 -0600

    initial commit

--- mre/main.c
+++ mre/main.c
@@ -5,10 +5,10 @@
 //  Created by Finxx1 on 2/19/24.
 //
 
-#include <stdio.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    glfwInit();
+    
     return 0;
 }
